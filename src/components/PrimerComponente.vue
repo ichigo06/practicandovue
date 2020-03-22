026756fe57cb561dd1712300d6a6f4438d1ff90a
Rogger <informaticoseoane@gmail.com>
2020-03-21 20:37:25 -0500

    Archivo de vue

---
+++ src/components/PrimerComponente.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+  <h1 ref="mainElement">{{message}}</h1>
+  <button @click="ChangeMessaged">Cambiar</button>
+  <button @click="$destroy()">Destruir Componente</button>
+</div>
+</template>
+<script>
+export default {
+    name:'PrimerComponent',
+    data () {
+      return {
+        message: 'Hola'
+      }
+    },
+    methods: {
+      ChangeMessaged() {
+        this.message = 'Hello'
+      }
+    },
+    beforeCreate () {
+      console.error('Estoy beforeCreate')
+      console.error(this.message)
+    },
+    created () {
+      console.error('Estoy en Create')
+      console.error(this.message)
+    },
+    beforeMount () {
+      console.log('Estoy en beforeMount')
+      console.log(this.$refs)
+      console.log(this.$refs.mainElement)
+    },
+    mounted() {
+      console.log('Estoy en mounted')
+      console.log(this.$refs)
+      console.log(this.$refs.mainElement)
+    },
+    beforeUpdate() {
+      console.log('Estoy en beforeUpdate')
+      console.log(this.$refs.mainElement)
+    },
+    updated() {
+      console.log('Estoy en update')
+      console.log(this.$refs.mainElement)
+    },
+    beforeDestroy() {
+      console.log('Estoy en beforeDestroy')
+      console.log(this.message)
+    },
+    destroyed() {
+      console.log('Estoy en update')
+      console.log(this.message)
+    },
+}
+</script>
+<style>
+
+</style>
---
+++ view/header.vue
@@ -0,0 +1,44 @@
+<template>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+      <header>
+        <h3>{{ title }}</h3>
+    </header>
+
+
+</body>
+</html>
+    
+</template>
+<script>
+export default {
+    data() {
+        return {
+            title: 'Bienvenidos a Media Impact'
+        }
+     }
+}
+</script>
+
+<style scoped>
+* {
+    margin: 0;
+    padding: 0;
+}
+    header {
+        
+        background-color: darkgrey;
+        padding: 15px;
+        height: 50px;
+        text-align: center;
+       
+        
+    }
+</style>
